071949ba7a87222170412f5d7ef36b413a6b6add
bin senpai <theislamicgamer101@gmail.com>
2017-12-17 12:13:35 -0500

    update some shit

--- baka/Controllers/MainController.cs
+++ baka/Controllers/MainController.cs
@@ -12,15 +12,16 @@ namespace baka.Controllers
 {
     public class MainController : BakaController
     {
-        [Route("/{url}/{name}/.{ext?}")]
+        [Route("/{url}/{name}.{ext?}")]
         [Route("/{url}/{name}")]
-        [Route("/{name}/.{ext?}")]
-        [Route("/{name}/")]
+        [Route("/{name}.{ext?}")]
+        [Route("/{name}")]
         [AcceptVerbs("GET")]
         public async Task<IActionResult> GetFileOrLink(string name, string ext, string url)
         {
             using (var context = new BakaContext())
             {
+                Console.WriteLine(name + " || " + ext);
                 var file = await context.Files.FirstOrDefaultAsync(u => u.ExternalId == name);
 
                 if (file == null || file.Deleted)
@@ -35,7 +36,7 @@ namespace baka.Controllers
                     return Json(new
                     {
                         success = false,
-                        error = "404 Not Found",
+                        error = "Object not found",
                         code = 404
                     });
                 }
@@ -50,6 +51,19 @@ namespace baka.Controllers
             }
         }
 
+        [Route("/{*url}")]
+        public async Task<IActionResult> Get404()
+        {
+            Response.StatusCode = 404;
+
+            return Json(new
+            {
+                success = false,
+                error = "404 Not Found",
+                code = 404
+            });
+        }
+
         [NonAction]
         private async Task<IActionResult> GetLink(string name, string ext, string url)
         {
@@ -57,7 +71,7 @@ namespace baka.Controllers
             {
                 var link = await context.Links.FirstOrDefaultAsync(u => u.ExternalId == name);
 
-                if(link == null)
+                if (link == null)
                     return null;
 
                 return Redirect(link.Destination);
